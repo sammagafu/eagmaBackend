42258d591187726e3270efe2e9b59cdcfa15a823
Samuel Magafu <smagafu@gmail.com>
2024-03-30 01:30:18 +0300

    updated filter by category to make the ui cool

--- nominees/urls.py
+++ nominees/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', NomineeListCreateView.as_view(), name='nominee-list-create'),
     path('<int:pk>/', NomineeDetailView.as_view(), name='nominee-detail'),
     path('votes/', VoteListCreateView.as_view(), name='vote-list-create'),
-    path('/category/<slug:category_slug>/', NomineeByCategoryAPIView.as_view(), name='nominee_by_category'),
+    path('category/<slug:category_slug>/', NomineeByCategoryAPIView.as_view(), name='nominee_by_category'),
     path('votes/<int:pk>/', VoteDetailView.as_view(), name='vote-detail'),
 ]
